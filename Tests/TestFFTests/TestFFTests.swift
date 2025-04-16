aa2467be0a856f813df3d3a87e88461cef4120b0
Junior <juniorquegu@gmail.com>
2025-04-16 08:55:23 -0500

    fake test

--- Tests/TestFFTests/TestFFTests.swift
+++ Tests/TestFFTests/TestFFTests.swift
@@ -14,4 +14,15 @@ final class TestFFTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
+    
+    func test2() throws {
+        
+        
+        XCTAssertTrue(5-5==0)
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
 }
