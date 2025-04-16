b10b7bc6bb885f76592893b1e6979152da8a6b6e
Junior Quevedo Gutierrez <juniorquegu@gmail.com>
2025-04-16 10:47:32 -0500

    Update TestFFTests.swift

--- Tests/TestFFTests/TestFFTests.swift
+++ Tests/TestFFTests/TestFFTests.swift
@@ -36,4 +36,15 @@ final class TestFFTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
+
+        func test4() throws {
+        
+        
+        XCTAssertTrue(5-5==0)
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
 }
